5ddd63140d79f2865404c8dc2c3db2e219e41292
gabriel011 <gabriel.santos@baymetrics.com.br>
2025-07-10 11:14:24 -0300

    update in docs for new template

--- sql/base/create.sql
+++ sql/base/create.sql
@@ -1,147 +1,138 @@
-CREATE TABLE sys_flow.owner (
-    own_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    own_last_name VARCHAR(60) NOT NULL,
-    own_value DECIMAL(7,2) NOT NULL,
-    own_cpf VARCHAR(14)  NULL,
-    own_email VARCHAR(60) NOT NULL,
-    own_password VARCHAR(128),
-    CONSTRAINT own_id PRIMARY KEY (own_id)
-);
-
-
-CREATE TABLE sys_flow.historic_of_out (
-    hout_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    hout_value DECIMAL(7,2) NOT NULL,
-    hout_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    hout_type_mov CHAR(1) NOT NULL,
-    own_id INTEGER NOT NULL,
-    CONSTRAINT hout_id PRIMARY KEY (hout_id),
-    FOREIGN KEY (own_id) REFERENCES sys_flow.owner(own_id) ON DELETE RESTRICT
-);
-
-CREATE TABLE sys_flow.company (
+CREATE TABLE IF NOT EXISTS ownership.tb_company(
     com_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    com_name VARCHAR(60) NOT NULL UNIQUE,
-    com_cnpj VARCHAR(18) NOT NULL UNIQUE,
-    com_start_date DATE DEFAULT CURRENT_DATE NOT NULL,
-    com_active BOOLEAN DEFAULT TRUE,
-    com_country CHAR(3) NOT NULL DEFAULT 'BRA',
+    com_created_at DATE DEFAULT CURRENT_DATE NOT NULL,
+    com_active BOOLEAN DEFAULT TRUE NOT NULL,
+    com_country CHAR(3) DEFAULT 'BRA' NOT NULL,
+    com_cnpj VARCHAR(18) UNIQUE NOT NULL,
+    com_name VARCHAR(60) UNIQUE NOT NULL,
     CONSTRAINT com_id PRIMARY KEY (com_id)
 );
 
-CREATE TABLE sys_flow.building (
-    bui_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    bui_name VARCHAR(48) NOT NULL UNIQUE,
-    bui_uf CHAR(2) NULL,
-    bui_city VARCHAR(48) NULL,
-    bui_district VARCHAR(48) NULL,
-    bui_street VARCHAR(48) NULL,
-    bui_number INTEGER NULL,
-    bui_yield DECIMAL(8,2) NULL,
-    bui_photo TEXT NULL,
-    own_id INTEGER NOT NULL,
-    com_id INTEGER NOT NULL,
-    CONSTRAINT bui_id PRIMARY KEY (bui_id),
-    FOREIGN KEY (com_id) REFERENCES sys_flow.company(com_id) ON DELETE RESTRICT,
-    FOREIGN KEY (own_id) REFERENCES sys_flow.owner(own_id) ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS ownership.tb_owner(
+	own_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	own_name VARCHAR(28) NOT NULL,
+	own_last_name VARCHAR(60) NOT NULL,
+	own_amount DECIMAL(8,2) NOT NULL,
+	own_password VARCHAR(128) NOT NULL,
+	own_cpf VARCHAR(14) UNIQUE NULL ,
+	own_email TEXT UNIQUE NOT NULL,
+	CONSTRAINT own_id PRIMARY KEY (own_id)
 );
 
-CREATE TABLE sys_flow.camera (
-    cam_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    cam_tag VARCHAR(12) NOT NULL UNIQUE,
-    cam_price_maintenance DECIMAL(5,2) NOT NULL,
-    bui_id INTEGER NOT NULL,
-    CONSTRAINT cam_id PRIMARY KEY (cam_id),
-    FOREIGN KEY (bui_id) REFERENCES sys_flow.building(bui_id) ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS ownership.tb_building(
+	bui_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	bui_name VARCHAR(48) NOT NULL,
+	bui_amount DECIMAL(8,2) NULL,
+	bui_photo TEXT NOT NULL,
+	own_id INTEGER NOT NULL,
+	CONSTRAINT bui_id PRIMARY KEY (bui_id),
+	FOREIGN KEY (own_id) REFERENCES ownership.tb_owner(own_id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-CREATE TABLE sys_flow.elevator (
-    ele_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    ele_tag VARCHAR(12) NOT NULL UNIQUE ,
-    ele_weight DECIMAL(4,2),
-    ele_number_person INTEGER,
-    ele_risk DECIMAL(2,2),
-    ele_amount_maintenance INTEGER,
-    bui_id INTEGER NOT NULL,
-    CONSTRAINT ele_id PRIMARY KEY (ele_id),
-    FOREIGN KEY (bui_id) REFERENCES sys_flow.building(bui_id) ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS ownership.tb_fluor(
+	flu_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	flu_created_at DATE DEFAULT CURRENT_DATE NOT NULL,
+	flu_end_of_contract DATE NULL,
+	flu_number INTEGER NOT NULL,
+	bui_id INTEGER NOT NULL,
+	com_id INTEGER NOT NULL,
+	CONSTRAINT flu_id PRIMARY KEY (flu_id),
+	FOREIGN KEY (com_id) REFERENCES ownership.tb_company ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (bui_id) REFERENCES ownership.tb_building ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-CREATE TABLE sys_flow.employee (
-    emp_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    emp_active BOOLEAN DEFAULT true,
-    emp_name VARCHAR(28) NOT NULL CHECK (char_length(emp_name) > 2),
-    emp_last_name VARCHAR(60) NOT NULL CHECK (char_length(emp_last_name) > 2),
-    emp_cpf VARCHAR(14) NOT NULL UNIQUE,
-    emp_photo TEXT,
-    emp_phone_number VARCHAR(15),
-    com_id INTEGER NOT NULL,
-    CONSTRAINT emp_id PRIMARY KEY (emp_id),
-    FOREIGN KEY (com_id) REFERENCES sys_flow.company(com_id) ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS ownership.tb_people(
+	peo_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	peo_name VARCHAR(28) NOT NULL,
+	peo_last_name TEXT NOT NULL,
+	peo_salario DECIMAL(8,2) NULL,
+	peo_cep VARCHAR(9) NULL,
+	peo_uf VARCHAR(48) NULL,
+	peo_town VARCHAR(48) NULL,
+	peo_district CHAR(2) NULL,
+	peo_street TEXT NULL,
+	peo_number INTEGER NULL,
+	peo_rg VARCHAR(12) NOT NULL,
+	peo_phone_number VARCHAR(15) NOT NULL,
+	peo_photo TEXT NOT NULL,
+	peo_cpf VARCHAR(14) NOT NULL,
+	com_id INTEGER NOT NULL,
+	CONSTRAINT peo_id PRIMARY KEY (peo_id),
+	FOREIGN KEY (com_id) REFERENCES ownership.tb_company ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE sys_flow.ass_tape (
-    tape_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    tape_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    cam_id INTEGER NOT NULL,
-    emp_id INTEGER NOT NULL,
-    CONSTRAINT tape_id PRIMARY KEY (tape_id),
-    FOREIGN KEY (cam_id) REFERENCES sys_flow.camera(cam_id) ON DELETE CASCADE,
-    FOREIGN KEY (emp_id) REFERENCES sys_flow.employee(emp_id) ON DELETE CASCADE
+
+CREATE TABLE IF NOT EXISTS equipment.tb_equipment(
+	equ_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	equ_type CHAR(1) NOT NULL,
+	equ_tag TEXT NOT NULL,
+	schema_ownership_flu_id INTEGER NOT NULL,
+	CONSTRAINT equ_id PRIMARY KEY (equ_id),
+	FOREIGN KEY (schema_ownership_flu_id)  REFERENCES ownership.tb_fluor(flu_id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-CREATE TABLE sys_flow.ratchat (
-    rat_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    rat_status CHAR(1) NOT NULL,
-    rat_tag VARCHAR(12) NOT NULL UNIQUE,
-    rat_price_maintenance DECIMAL(5,2) NULL,
-    CONSTRAINT rat_id PRIMARY KEY (rat_id)
+CREATE TABLE IF NOT EXISTS equipment.tb_capture(
+	cap_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	cap_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	cap_content TEXT NULL,
+	equ_id INTEGER NOT NULL,
+	CONSTRAINT cap_id PRIMARY KEY (cap_id),
+	FOREIGN KEY (equ_id) REFERENCES equipment.tb_equipment(equ_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE sys_flow.fluor (
-    fluor_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    fluor_start_date DATE DEFAULT CURRENT_DATE,
-    fluor_final_date DATE DEFAULT CURRENT_DATE,
-    fluor_number INTEGER NOT NULL,
-    com_id INTEGER NOT NULL,
-    bui_id INTEGER NOT NULL,
-    CONSTRAINT fluor_id PRIMARY KEY (fluor_id),
-    FOREIGN KEY (com_id) REFERENCES sys_flow.company(com_id) ON DELETE CASCADE,
-    FOREIGN KEY (bui_id) REFERENCES sys_flow.building(bui_id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS equipment.tb_token(
+	tok_id INTEGER  GENERATED ALWAYS AS IDENTITY,
+	tok_in BOOLEAN NOT NULL,
+	tok_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ,
+	tok_content TEXT NULL,
+	equ_id INTEGER NOT NULL,
+	schema_ownership_peo_id INTEGER NOT NULL,
+	CONSTRAINT tok_id PRIMARY KEY (tok_id),
+	FOREIGN KEY (equ_id) REFERENCES equipment.tb_equipment(equ_id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	FOREIGN KEY (schema_ownership_peo_id) REFERENCES ownership.tb_people(peo_id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-CREATE TABLE sys_flow.ass_recording_acess (
-    rec_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    rec_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    emp_id INTEGER NOT NULL,
-    rat_id INTEGER NOT NULL,
-    CONSTRAINT rec_id PRIMARY KEY (rec_id),
-    FOREIGN KEY (emp_id) REFERENCES sys_flow.employee(emp_id) ON DELETE CASCADE,
-    FOREIGN KEY (rat_id) REFERENCES sys_flow.ratchat(rat_id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS equipment.tb_elevator(
+	ele_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	ele_weight DECIMAL(7,2) NULL,
+	ele_risk DECIMAL(2,2) NULL,
+	ele_tag TEXT NOT NULL,
+	schema_ownership_bui_id INTEGER NOT NULL,
+	CONSTRAINT ele_id PRIMARY KEY (ele_id),
+	FOREIGN KEY (schema_ownership_bui_id) REFERENCES ownership.tb_building(bui_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- Estrutura polimórfica para equipamentos
--- mai_type: 'E' = Elevator, 'R' = Ratchat, 'C' = Camera
-
-CREATE TABLE sys_flow.maintenance (
-    mai_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    mai_description TEXT,
-    mai_total_price DECIMAL(5,2),
-    mai_start_date DATE DEFAULT CURRENT_DATE NOT NULL,
-    mai_date_prevision DATE,
-    mai_status CHAR(1) NOT NULL,
-    mai_id_equipment INTEGER NOT NULL,
-    mai_type CHAR(1) NOT NULL,
-    CONSTRAINT mai_id PRIMARY KEY (mai_id)
-    -- Integridade polimórfica tratada na aplicação
+CREATE TABLE IF NOT EXISTS equipment.tb_point_of_stopover(
+	poi_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	poi_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	ele_id INTEGER NOT NULL,
+	schema_ownership_flu_id INTEGER NOT NULL,
+	CONSTRAINT poi_id PRIMARY KEY (poi_id),
+	FOREIGN KEY (ele_id) REFERENCES equipment.elevator(ele_id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	FOREIGN KEY (schema_ownership_flu_id) REFERENCES ownership.tb_fluor(flu_id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-CREATE TABLE sys_flow.ass_point_of_stopover(
-    poi_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    poi_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ele_id INTEGER NOT NULL,
-    fluor_id INTEGER NOT NULL,
-    CONSTRAINT poi_id PRIMARY KEY (poi_id),
-    FOREIGN KEY (ele_id) REFERENCES sys_flow.elevator(ele_id) ON DELETE CASCADE,
-    FOREIGN KEY (fluor_id) REFERENCES sys_flow.fluor(fluor_id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS equipment.tb_maintenance(
+	mai_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	mai_description TEXT NULL,
+	mai_total_price DECIMAL(8,2),
+	mai_created_at DATE DEFAULT CURRENT_DATE NOT NULL,
+	mai_finish_at DATE NULL,
+	mai_status CHAR(1) DEFAULT 'Q' NOT NULL,
+	mai_type CHAR(1) NOT NULL,
+	mai_id_equipment INTEGER NOT NULL,
+	CONSTRAINT mai_id PRIMARY KEY (mai_id),
+	FOREIGN KEY (mai_id_equipment) REFERENCES equipment.tb_equipment(equ_id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	FOREIGN KEY (mai_id_equipment) REFERENCES equipment.tb_elevator(ele_id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS ownership.tb_statement(
+	sta_id INTEGER GENERATED ALWAYS AS IDENTITY,
+	sta_amount DECIMAL(8,2) NOT NULL,
+	sta_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	own_id INTEGER NOT NULL,
+	schema_equipment_mai_id INTEGER NULL,
+	CONSTRAINT sta_id PRIMARY KEY (sta_id),
+	FOREIGN KEY (own_id) REFERENCES ownership.tb_owner(own_id)  ON DELETE RESTRICT ON UPDATE RESTRICT,
+	FOREIGN KEY (schema_equipment_mai_id) REFERENCES equipment.tb_maintenance(mai_id)  ON DELETE RESTRICT ON UPDATE RESTRICT
 );
